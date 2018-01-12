5c502f7c95a76838ec2694d683319a1c065b7b47
qh <qianhu@pindous.com>
2018-01-11 16:41:38 -0800

    makes logic working of JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 
 $(document).ready(function () {
-  $("").submit(function () {
+  $(".blanks").submit(function (event) {
      event.preventDefault();
+
+    var flavor1 = $("input:radio[name=flavor1]:checked").val();
+    var flavor2 = $("input:radio[name=flavor2]:checked").val();
+    var flavor3 = $("input:radio[name=flavor3]:checked").val();
+
+    var result = "";
+    if ((flavor1 === "salad") && (flavor2 === "pasta")) {
+       result += "you are a dog person";
+       alert("dog");
+    } else if ((flavor2 === "pasta") && (flavor3 ==="juice")) {
+      result += "you are a cat person";
+    } else {
+      result += "you are a sheep person";
+    }
+
+    $("#output").text(result);
   });
-var flavor1 = $("input:radio[name=flavor1]:checked").val();
-var flavor2 = $("input:radio[name=flavor2]:checked").val();
-var flavor3 = $("input:radio[name=flavor3]:checked").val();
-
-
 });
